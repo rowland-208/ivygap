fc1c017348a97b67fbe0a47db65ce317c5b415b9
James <rowland.208@gmail.com>
2020-11-12 20:54:42 -0800

    Perform analysis for individual vs other

--- analysis.py
+++ analysis.py
@@ -15,44 +15,43 @@ lb.quant_sf = lb.quant_sf.apply(lambda s: '_'.join(s.split('_')[:4]))
 lb = lb.set_index('quant_sf')
 
 X = ft.values
-y = ft.index.map(lb.structure_name).values
+y0 = ft.index.map(lb.structure_name).values
+for label, y in (
+        ('All', y0),
+        *((key, np.where(y0==key, key, 'Other')) for key in np.unique(y0))):
 
-clf = ExtraTreesClassifier(
-    n_estimators=500, random_state=2,
-    max_depth=10, criterion='entropy',
-    min_impurity_decrease=0.05)
-clf.fit(X, y)
-print('Score: ', clf.score(X, y))
+    clf = ExtraTreesClassifier(
+        n_estimators=500, random_state=2,
+        max_depth=10, criterion='entropy',
+        min_impurity_decrease=0.05)
+    clf.fit(X, y)
+    print('Score: ', clf.score(X, y))
 
-dimred = SelectFromModel(clf, prefit=True, max_features=50)
-X_new = dimred.transform(X)
+    dimred = SelectFromModel(clf, prefit=True, max_features=50)
+    X_new = dimred.transform(X)
 
-tsne = TSNE(
-    n_components=3,
-    n_iter=10000, learning_rate=40.,
-    early_exaggeration=20.,
-    perplexity=30., random_state=0)
-X_3d = tsne.fit_transform(X_new, y)
-X_min = np.min(X_3d, axis=0)
-X_max= np.max(X_3d, axis=0)
-X_3d = (X_3d - X_min)/(X_max - X_min)
+    tsne = TSNE(
+        n_components=3,
+        n_iter=10000, learning_rate=40.,
+        early_exaggeration=20.,
+        perplexity=30., random_state=0)
+    X_3d = tsne.fit_transform(X_new, y)
+    X_min = np.min(X_3d, axis=0)
+    X_max= np.max(X_3d, axis=0)
+    X_3d = (X_3d - X_min)/(X_max - X_min)
 
-df = pd.DataFrame(
-    np.transpose([
-        ft.index,
-        ft.index.map(lb.structure_name),
-        *(X_3d.T)]),
-    columns=['label', 'structure', 'x1', 'x2', 'x3'])
+    df = pd.DataFrame(
+        np.transpose([
+            ft.index,
+            ft.index.map(lb.structure_name),
+            *(X_3d.T)]),
+        columns=['label', 'structure', 'x1', 'x2', 'x3'])
 
-fig = px.scatter_3d(
-    df, x='x1', y='x2', z='x3', 
-    color='structure',
-    hover_name='label')
-fig.write_html('structures.html')
+    fig = px.scatter_3d(
+        df, x='x1', y='x2', z='x3', 
+        color='structure',
+        hover_name='label')
+    fig.write_html('structures-{}.html'.format(label))
 
-out = ft.T.index[dimred.get_support()]
-
-for args in zip(
-        out[0::5], out[1::5],
-        out[2::5], out[3::5], out[4::5]):
-    print('{} {} {} {} {}'.format(*args))
+    with open('expressions-{}.csv'.format(label), 'w') as fp:
+        fp.write(','.join(ft.T.index[dimred.get_support()]))
